9eabf129d3dd7b777fc8e02688f13e885dc65e9d
pkulej <przemek.kulej1@gmail.com>
2024-01-19 18:52:24 +0100

    chore(.gitignore): add .env file to the gitignore to prevent sensitive information from being committed feat(get-categories.tsx): add a new action to fetch categories from the API feat(page.tsx): create a new page component for the home page feat(main-nav.tsx): create a new component for the main navigation menu feat(footer.tsx): create a new component for the footer feat(navbar-actions.tsx): create a new component for the actions in the navbar feat(navbar.tsx): create a new component for the navbar feat(button.tsx): create a new component for the button feat(container.tsx): create a new component for the container feat(globals.css): add global styles for the app feat(layout.tsx): create a new layout component to wrap the pages chore(package.json): add new dependencies for the project feat(types.ts): add types for the billboard and category objects

---
+++ app/components/navbar-actions.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ShoppingBag } from "lucide-react";
+import Button from "./ui/button";
+
+const NavbarActions = () => {
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      NavbarActions
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white" />
+      </Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
