f7e3a9c9a522ce32b255fb4d1e08458c13c33777
pkulej <przemek.kulej1@gmail.com>
2024-01-20 23:56:07 +0100

    feat: add get-billboard and get-products actions
    - Added new files `get-billboard.tsx` and `get-products.tsx` to handle API requests for billboards and products respectively.
    - These actions use the `fetch` function to make HTTP requests to the specified API endpoints.
    - The `getBillboard` function retrieves a single billboard by its ID.
    - The `getProducts` function retrieves a list of products based on optional query parameters such as category ID, color ID, size ID, and whether the product is featured.
    - The API URL is obtained from the `NEXT_PUBLIC_API_URL` environment variable.
    
    feat: add Billboard component
    
    - Added new file `billboard.tsx` to define the `Billboard` component.
    - The `Billboard` component displays a billboard image with a label.
    - The image URL and label are obtained from the `data` prop.
    - The component uses Tailwind CSS classes for styling.
    
    feat: update HomePage component
    
    - Imported the `getBillboard` and `getProducts` actions in the `HomePage` component.
    - Added a container component and a billboard component to the `HomePage` component.
    - The `HomePage` component fetches the featured products and a specific billboard using the respective actions.
    - The fetched data is passed as props to the `Billboard` and `ProductList` components.
    - The `revalidate` variable is set to 0 to disable automatic revalidation of the page.
    
    feat: add ProductList and NoResults components
    
    - Added new files `product-list.tsx` and `no-result.tsx` to define the `ProductList` and `NoResults` components respectively.
    - The `ProductList` component displays a list of products with a title.
    - The `NoResults` component displays a message when no results are found.
    
    fix: update NavbarActions component
    
    - Added a state variable `isMounted` to track whether the component is mounted.
    - Used the `useEffect` hook to set `isMounted` to `true` when the component is mounted.
    - Added a conditional rendering to return `null` if the component is not mounted.
    - Updated the JSX to include a span element displaying the number of items in the shopping bag.
    
    chore: update package.json
    
    - Added the `query-string` package as a dependency in the `package.json` file.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import Container from "../components/ui/container";
+import Billboard from "../components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "../components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("61400411-23bf-407d-87aa-8998c2eb4c50");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10 ">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
