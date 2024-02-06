3bf658679704dead24bcc3ec1a366e06449f98a1
pkulej <przemek.kulej1@gmail.com>
2024-02-06 17:26:31 +0100

    Fixed button location

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -41,9 +41,9 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
-          <ShoppingCart onClick={onAddToCart} size={20} />
+          <ShoppingCart size={20} />
         </Button>
       </div>
     </div>
